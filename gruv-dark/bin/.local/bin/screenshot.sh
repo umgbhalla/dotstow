5f59f7c6912edfee40ef87ee14c258219fee3018
Umang Bhalla <67634565+umgbhalla@users.noreply.github.com>
2021-08-28 03:23:44 +0530

    misc changes to configs

--- gruv-dark/bin/.local/bin/screenshot.sh
+++ gruv-dark/bin/.local/bin/screenshot.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 p="/tmp/$(date +"%d-%m-%y-%M").png"
-# c=$(echo -en "Window\0icon\x1fappointment-new\nSelection\0icon\x1fimage-crop\nScreen\0icon\x1fvideo-display\n" \
-# 	| rofi -dmenu -theme screenshot -p "")
-# sleep 0.2
+c=$(echo -en "Window\0icon\x1fappointment-new\nSelection\0icon\x1fimage-crop\nScreen\0icon\x1fvideo-display\n" \
+	| rofi -dmenu -theme screenshot -p "")
+sleep 0.2
 
-# case $c in
-#   "Window")
-#     import -window $(xwininfo | grep -Po "Window id: \K\w+") $p
-#     ;;
-#   "Selection")
-#     import $p 
-#     ;;
-#   "Screen")
+case $c in
+  "Window")
+  maim -u -o -s $p
+    ;;
+  "Selection")
+  maim -u -o -s $p
+    ;;
+  "Screen")
    maim -u -o $p
-    # ;;
-  # *)
-    # exit
-    # ;;
-# esac
+    ;;
+  *)
+    exit
+    ;;
+esac
 
 xclip -selection clipboard -target image/png -i $p
 
