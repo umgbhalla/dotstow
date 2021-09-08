306668ff23806a90abdf1217648cdfc317d3d343
Umang Bhalla <67634565+umgbhalla@users.noreply.github.com>
2021-09-08 22:07:32 +0530

    moun-tain: reconfigured and new scripts

--- moun-tain/polybar/.config/polybar/launch.sh
+++ moun-tain/polybar/.config/polybar/launch.sh
@@ -29,19 +29,23 @@ hideIt.sh --name '^Polybar tray window$' --region 0x0+25+25  --peek 9 -d left -i
 echo "########################################################################"
 echo "hiding systray "
 
+
 # hideIt.sh --name '^polybar-sub_eDP$' --region 0x1060+1920+20 --peek 2 -d bottom -i 0.2   & 
-hideIt.sh --name '^polybar-sub_eDP$'  --peek 8 -d bottom -i 0.2  --hover & 
-echo "########################################################################"
-echo "hiding botton bar "
+# hideIt.sh --name '^polybar-sub_eDP$'  --peek 8 -d bottom -i 0.2  --hover & 
+# echo "########################################################################"
+# echo "hiding bottom bar "
+
+
 # hideIt.sh --name '^polybar-main_eDP$' -H --peek 6 -d top -i 0.1 &
 # echo "########################################################################"
 # echo "hiding main bar "
+
 
 # while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 sleep 6
 # while ! pgrep -x polybar >/dev/null; do sleep 1; done
 xdo raise -a "Polybar tray window"
-xdo raise -a "polybar-sub_eDP"
+# xdo raise -a "polybar-sub_eDP"
 # xdo raise -a "polybar-main_eDP"
 echo "########################################################################"
 echo "raised bar"
