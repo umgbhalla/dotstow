685ba9034177254c9f6779b9dcb5db235464d8a6
Umang Bhalla <67634565+umgbhalla@users.noreply.github.com>
2021-10-27 01:04:45 +0530

    fix(polybar): fix hover reliability of systray and main bar

--- cr0nus/polybar/.config/polybar/launch.sh
+++ cr0nus/polybar/.config/polybar/launch.sh
@@ -15,15 +15,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
 # Launch all bars :D
-polybar -c ~/.config/polybar/config.ini systray -r &
-echo "########################################################################"
-echo "systray launched"
-polybar -c ~/.config/polybar/config.ini sub -r &
-echo "########################################################################"
-echo "sub bar launched"
 polybar -c ~/.config/polybar/config.ini main -r &
 echo "########################################################################"
 echo "main bar launched"
+polybar -c ~/.config/polybar/config.ini sub -r &
+echo "########################################################################"
+echo "sub bar launched"
+# polybar -c ~/.config/polybar/config.ini systray -r &
+# echo "########################################################################"
+# echo "systray launched"
 
 
 
@@ -34,16 +34,22 @@ echo "hiding sub bar "
 hideIt.sh --name '^polybar-main_eDP$' -w  --peek 4 -d bottom -i 1  --hover & 
 echo "########################################################################"
 echo "hiding main bar "
-hideIt.sh --name '^Polybar tray window$' -w --region 0x1060+55+25  --peek 35 -d left -i 0.1 & 
+hideIt.sh --name '^Polybar tray window$' -w --region 0x1060+55+55  --peek 4 -d bottom -i 0.1 & 
+# hideIt.sh --name '^Polybar tray window$' -w  --peek 4 -d bottom -i 0.1 --hover & 
 echo "########################################################################"
 echo "hiding systray "
 
 
+
+
 # Wait wait until all bars are loaded and hidden
 while ! pgrep -x polybar >/dev/null; do sleep 1; done
-
 # just making sure
 sleep 6
+
+
+
+
 # Raise all bars
 xdo raise -a "polybar-main_eDP"
 xdo raise -a "polybar-sub_eDP"
