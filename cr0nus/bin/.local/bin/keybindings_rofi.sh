9c5b82015250c7c0296e38fec6cba60a7da239b8
Umang Bhalla <umg.bhalla88@gmail.com>
2021-11-05 19:30:24 +0530

    chore(base)

--- cr0nus/bin/.local/bin/keybindings_rofi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
-    column -t -s $'\t' |
-    rofi -dmenu -i -p "keybindings:" -markup-rows \
-	-no-show-icons \
-	-width 26 \
-	-lines 14 \
-	-padding 20
