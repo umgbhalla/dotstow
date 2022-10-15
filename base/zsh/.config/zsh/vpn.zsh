74cb71c1a46854ff5e92fe9bdb8a4645c6fefc8e
umgbhalla <umg.bhalla88@gmail.com>
2022-10-15 21:18:49 +0530

    misc: dump

--- base/zsh/.config/zsh/vpn.zsh
+++ base/zsh/.config/zsh/vpn.zsh
@@ -1,15 +1,15 @@
 # vpn_config_file=$HOME/downloads/client.ovpn
 vpn_config_file=$HOME/downloads/profile-14.ovpn
-vpn_username="umang.bhalla"
+# vpn_username="umang.bhalla"
 # vpn_password_file=
 # vpn_password="4a=Fyb"
-vpn_password="hKOI0Mrn5eIP"
+# vpn_password="hKOI0Mrn5eIP"
 
 
-vpnup() {
-    # Connect to VPN
-printf "%s\n%s\n" "$vpn_username" "$vpn_password"  | openvpn3 session-start --config "$vpn_config_file" 
-}
+# vpnup() {
+#     # Connect to VPN
+# printf "%s\n%s\n" "$vpn_username" "$vpn_password"  | openvpn3 session-start --config "$vpn_config_file" 
+# }
 
 vpnls() {
     # List of VPN sessions
@@ -40,22 +40,23 @@ else
   echo "VPN 🔴 -> 🟢 "
    # read -p "Start VPN?: " -n 1 -r 
    #    if [[ $REPLY == ["Yy"] ]]; then
-python -c "$( cat << EOF
-import pexpect
-from pexpect import popen_spawn
-commands = "openvpn3 session-start --config /home/umang/downloads/profile-14.ovpn"
-commands_list = commands.split(" ")
-username = "umang.bhalla"
-password = "hKOI0Mrn5eIP"
-
-session = pexpect.popen_spawn.PopenSpawn(commands)
-session.expect("Auth User name: ")
-session.sendline(username)
-session.expect("Auth Password: ")
-session.sendline(password)
-print("Connected...")
-EOF
-)"
+  openvpn3 session-start --config /home/umang/downloads/profile-14.ovpn
+# python -c "$( cat << EOF
+# import pexpect
+# from pexpect import popen_spawn
+# commands = "openvpn3 session-start --config /home/umang/downloads/profile-14.ovpn"
+# commands_list = commands.split(" ")
+# username = "umang.bhalla"
+# password = "hKOI0Mrn5eIP"
+#
+# session = pexpect.popen_spawn.PopenSpawn(commands)
+# session.expect("Auth User name: ")
+# session.sendline(username)
+# session.expect("Auth Password: ")
+# session.sendline(password)
+# print("Connected...")
+# EOF
+# )"
       # fi
 fi
 
